79c1e527190d424a535e12fcc293cdf8f2ff7871
Iryna Bryzhan <iryna97bryzhan@gmail.com>
2024-03-27 10:18:30 +0200

    Homework_1

--- Homework-1/index.js
+++ Homework-1/index.js
@@ -1,10 +1,3 @@
-// Використовуючи alert та prompt створити пешу програму за таким функціоналом:
-
-// Привітатись з юзером - "Hello!";
-// Спитати імя - "What is your name?";
-// Спитати з якої він країни - "Where are you from?";
-// Вивести на екран - "Nice to meet you, {name}! You are from {country}!"
-
 alert('Hello!');
 const yourName = prompt('What is your name?', '');
 const country = prompt('Where are you from?', '');
