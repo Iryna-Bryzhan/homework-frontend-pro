ebac8cd2d249b7aee09af5ceaf6783e130fbce1e
Iryna Bryzhan <iryna97bryzhan@gmail.com>
2024-06-04 17:50:13 +0300

    Homework 20.2

--- Homework-20.2/code/app/js/utils/utils.js
+++ Homework-20.2/code/app/js/utils/utils.js
@@ -1,7 +1,6 @@
 function sum(a, b) {
     return a + b
 }
-
 const log = () => {
     console.log('hello world')
 }
--- Homework-20.2/code/gulpfile.js
+++ Homework-20.2/code/gulpfile.js
@@ -1,155 +1,3 @@
-// const gulp = require("gulp");
-//
-// gulp.task("sayHello", async () => {
-//   console.log("hello custom gulp");
-// });
-//
-// gulp.task("default", async () => {
-//   console.log("hello default gulp");
-// });
-
-/****************************************
- * compose
- ****************************************/
-
-// const { parallel, series } = require("gulp");
-
-// function foo(cb) {
-//   console.log('foo')
-//   cb()
-// }
-//
-// function bar(cb) {
-//   console.log('bar')
-//   cb()
-// }
-//
-// exports.build = series(foo, bar);
-// exports.default = parallel(foo, bar);
-
-// function clean(cb) {
-//   console.log('Cleaning...')
-//   cb()
-// }
-//
-// function cssCompilation(cb) {
-//   console.log('css compile...')
-//   cb()
-// }
-//
-// function cssMinify(cb) {
-//   console.log('css minify...')
-//   cb()
-// }
-//
-// function jsTranspile(cb) {
-//   console.log('js transp...')
-//   cb()
-// }
-//
-// function jsMinify(cb) {
-//   console.log('js minify...')
-//   cb()
-// }
-//
-// function publish(cb) {
-//   console.log('publish it')
-//   cb()
-// }
-//
-// exports.build = series(
-//     clean,
-//     parallel( cssCompilation, cssMinify),
-//     parallel( jsTranspile, jsMinify ),
-//     publish
-// )
-
-/****************************************
- * plugins
- ****************************************/
-
-// const { src, dest } = require("gulp");
-// const uglify = require("gulp-uglify");
-// const rename = require("gulp-rename");
-//
-// const path = "app/js";
-//
-// exports.default = function () {
-//   return src(`${path}/*.js`)
-//     .pipe(uglify())
-//     .pipe(rename({ extname: ".min.js" }))
-//     .pipe(dest("dist/"));
-// };
-
-/****************************************
- *
- ****************************************/
-// const { src, dest, watch, series } = require("gulp");
-// const uglify = require("gulp-uglify");
-// const rename = require("gulp-rename");
-//
-// const path = "app/js";
-//
-// function scripts() {
-//     return src(`${path}/*.js`)
-//     .pipe(uglify())
-//     .pipe(rename({ extname: ".min.js" }))
-//     .pipe(dest("dist/"));
-// }
-//
-// function watcher() {
-//   watch(`${path}/*.js`, scripts)
-// }
-//
-// exports.default = series(watcher)
-
-/****************************************
- *
- ****************************************/
-
-// const { src, dest, watch, series } = require("gulp");
-// const uglify = require("gulp-uglify");
-// const rename = require("gulp-rename");
-// const clean = require("gulp-clean");
-// const sass = require("sass");
-// const gulpSass = require("gulp-sass");
-// const scss = gulpSass(sass);
-//
-// const BUILD_FOLDER = "./dist";
-// const SCRIPT_FOLDER = "./app/js";
-// const STYLES_FOLDER = "./app/styles";
-//
-// async function cleanDist() {
-//   return clean(`${BUILD_FOLDER}/**/*`, { force: true });
-// }
-//
-// function scripts() {
-//   return src(`${SCRIPT_FOLDER}/*.js`)
-//     .pipe(uglify())
-//     .pipe(rename({ extname: ".min.js" }))
-//     .pipe(dest("dist/"));
-// }
-//
-// function styles() {
-//   return src(`${STYLES_FOLDER}/*.scss`)
-//     .pipe(scss({ outputStyle: "compressed" }).on("error", scss.logError))
-//     .pipe(rename({ extname: ".min.css" }))
-//     .pipe(dest("dist/"));
-// }
-//
-// function watcher() {
-//   watch(`${SCRIPT_FOLDER}/*.js`, scripts);
-//   watch(`${STYLES_FOLDER}/*.scss`, styles);
-// }
-//
-// exports.build = series(cleanDist, scripts, styles);
-// exports.default = series(scripts, styles, watcher);
-
-
-/****************************************
- *
- ****************************************/
-
 const { src, dest, watch, series, parallel } = require("gulp");
 const rename = require("gulp-rename");
 const uglify = require("gulp-uglify");
@@ -187,10 +35,12 @@ function html() {
 
 function scripts() {
   return src([`${UTILS_FOLDER}*.js`, `${SCRIPTS_FOLDER}*.js`])
-    .pipe(babel({
-      presets: ["@babel/preset-env"],
-      plugins: ["@babel/plugin-transform-modules-commonjs"],
-    }))
+    .pipe(
+      babel({
+        presets: ["@babel/preset-env"],
+        plugins: ["@babel/plugin-transform-modules-commonjs"],
+      })
+    )
     .pipe(concat("main.js"))
     .pipe(uglify())
     .pipe(rename({ extname: ".min.js" }))
@@ -217,8 +67,8 @@ function images() {
 function watcher() {
   browserSync.init({
     server: {
-      baseDir: BUILD_FOLDER
-    }
+      baseDir: BUILD_FOLDER,
+    },
   });
   watch(`${SCRIPTS_FOLDER}*.js`, scripts);
   watch(`${STYLES_FOLDER}*.scss`, styles);
